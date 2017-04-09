99f6cc6aec5645851c6ce85141dd31e6101c5aca
swati-93 <swati.1993cs@gmail.com>
2017-04-09 12:08:27 +0530

    initial commit

---
+++ app/src/main/java/swatkez/github/io/fitbitdata/MainActivity.java
@@ -0,0 +1,13 @@
+package swatkez.github.io.fitbitdata;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
